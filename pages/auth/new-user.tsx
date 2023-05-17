328af9b883b8d5c459f439252a038c7f7bd2aa47
dejmedus <email96123@gmail.com>
2023-05-17 12:01:09 -0600

    create initial onboarding form

--- pages/auth/new-user.tsx
+++ pages/auth/new-user.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
-import Router from "next/router";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function NewUser() {
+  const router = useRouter();
   const [isOrg, setIsOrg] = useState(false);
+  const [orgName, setOrgName] = useState("");
+  const [orgDesc, setOrgDesc] = useState("");
 
   const handleForm = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     try {
-      const body = { isOrg };
+      const body = { isOrg, orgName, orgDesc };
       await fetch(`/api/onboarding`, {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify(body),
       });
-      // await Router.push("/");
+      await router.push("/");
     } catch (error) {
       console.error(error);
     }
@@ -21,17 +27,48 @@ export default function NewUser() {
 
   return (
     <>
-      <h1>Welcome to Image Vault!</h1>
-      <form onSubmit={handleForm}>
-        <label htmlFor="role">Are you an organization?</label>
-        <input
-          id="role"
-          onChange={() => setIsOrg(!isOrg)}
-          checked={isOrg}
-          type="checkbox"
+      <Head>
+        <title>Welcome | Image Vault</title>
+        <meta
+          name="description"
+          content="Find and share images with the world."
         />
-        <button>Submit</button>
-      </form>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Welcome to Image Vault!</h1>
+        <form onSubmit={handleForm}>
+          <label htmlFor="role">Are you an organization?</label>
+          <input
+            id="role"
+            onChange={() => setIsOrg(!isOrg)}
+            checked={isOrg}
+            type="checkbox"
+          />
+          {isOrg ? (
+            <>
+              <label htmlFor="name">Organization name</label>
+              <input
+                id="name"
+                onChange={(e) => setOrgName(e.target.value)}
+                value={orgName}
+                type="text"
+              />
+              <label htmlFor="desc">Tell us about your organization</label>
+              <textarea
+                onChange={(e) => setOrgDesc(e.target.value)}
+                value={orgDesc}
+                name="desc"
+                id=""
+                cols={30}
+                rows={10}
+              ></textarea>
+            </>
+          ) : null}
+          <button>Submit</button>
+        </form>
+      </main>
     </>
   );
 }

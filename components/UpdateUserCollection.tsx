1c1eaad02c79502821611c27485ebf74a9c9a14d
James Lee <jamesleex86@gmail.com>
2023-05-18 13:35:38 -0700

    Passed in fake-props

---
+++ components/UpdateUserCollection.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+interface UserCollectionProps {
+    userImages: { id: number, image: string }
+}
+
+
+const UpdateUserCollection: React.FC<UserCollectionProps> = (props) => {
+
+
+
+
+    return <div>
+        {/* {props.userImages.map(image => (
+
+        ))} */}
+
+    </div>
+}
+
+
+export default UpdateUserCollection;

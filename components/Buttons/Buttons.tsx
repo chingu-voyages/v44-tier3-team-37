36f83c959e5205f9849d56aeedbe06547004dc47
dejmedus <email96123@gmail.com>
2023-05-18 15:37:42 -0600

    add disable button functionality & styling

--- components/Buttons/Buttons.tsx
+++ components/Buttons/Buttons.tsx
@@ -4,11 +4,17 @@ interface IButton {
   children: React.ReactNode;
   onClick?: any;
   type?: "submit";
+  disabled?: boolean;
 }
 
-export function Btn({ children, type, onClick }: IButton) {
+export function Btn({ children, type, disabled, onClick }: IButton) {
   return (
-    <button type={type || "button"} className={styles.common} onClick={onClick}>
+    <button
+      type={type || "button"}
+      className={styles.common}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );

d0fad5bc2a77c6f7e15c56d378fef5f901f8e911
Jordan Roberts <jrobertswebdev@gmail.com>
2023-05-24 17:34:57 -0500

    Add orgId to image on upload

--- pages/api/image/index.ts
+++ pages/api/image/index.ts
@@ -1,27 +1,43 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '@/lib/prisma'
-import { Tag } from '@/pages/upload'
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/prisma";
+import { Tag } from "@/pages/upload";
 
 type ImageData = {
-  user: string
-  title: string
-  location: string
-  date: string
-  description: string
-  alt: string
-  url: string
-  tagIds: string[]
-}
+  orgId: string;
+  title: string;
+  location: string;
+  date: string;
+  description: string;
+  alt: string;
+  url: string;
+  tagIds: string[];
+};
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
     try {
-      const { title, location, date, description, alt, url, tagIds, user }: ImageData = req.body
-      const safeDate = date != '' ? new Date(date) : null
+      const {
+        title,
+        location,
+        date,
+        description,
+        alt,
+        url,
+        tagIds,
+        orgId,
+      }: ImageData = req.body;
+      const safeDate = date != "" ? new Date(date) : null;
 
-      const image = await prisma.image.create({
+      await prisma.image.create({
         data: {
-          userId: user,
+          organization: {
+            connect: {
+              id: orgId,
+            },
+          },
           title: title,
           location: location,
           date: safeDate,
@@ -32,14 +48,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             connect: tagIds.map((id) => ({ id: id })),
           },
         },
-      })
+      });
 
-      res.status(200).json({ message: 'Image data saved successfully' })
+      res.status(200).json({ message: "Image data saved successfully" });
     } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: 'Error saving image data' })
+      console.error(error);
+      res.status(500).json({ message: "Error saving image data" });
     }
   } else {
-    res.status(405).json({ message: 'Method not allowed' })
+    res.status(405).json({ message: "Method not allowed" });
   }
 }
--- pages/upload/index.tsx
+++ pages/upload/index.tsx
@@ -1,68 +1,75 @@
-import { useState, useRef, use, useEffect } from 'react'
-import prisma from '@/lib/prisma'
-import { useS3Upload } from 'next-s3-upload'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { useState, useRef, use, useEffect } from "react";
+import prisma from "@/lib/prisma";
+import { useS3Upload } from "next-s3-upload";
+import { NextApiRequest, NextApiResponse } from "next";
+import type { NextAuthOptions, Session } from "next-auth";
+import { getServerSession } from "next-auth";
 
-import s from './Upload.module.css'
-import { Btn, BoldBtn } from '@/components/Buttons/Buttons'
-import Dropzone from '@/components/Upload/Dropzone'
-import Tags from '@/components/Upload/Tags'
-import { useSession } from 'next-auth/react'
+import s from "./Upload.module.css";
+import { Btn, BoldBtn } from "@/components/Buttons/Buttons";
+import Dropzone from "@/components/Upload/Dropzone";
+import Tags from "@/components/Upload/Tags";
+import { useSession } from "next-auth/react";
 
-import { Loader } from '@/components/Upload/Loader'
+import { Loader } from "@/components/Upload/Loader";
+import { authOptions } from "../api/auth/[...nextauth]";
 
 export type Tag = {
-  name: string
-  id: string
-}
+  name: string;
+  id: string;
+};
 
 export type TagWithImages = {
-  name: string
-  id: string
-  imageIds: string[]
-}
+  name: string;
+  id: string;
+  imageIds: string[];
+};
 
-export default function Upload() {
-  const { data: session } = useSession()
-  const [allTags, setAllTags] = useState<TagWithImages[]>([])
-  const [files, setFiles] = useState<File[]>([])
-  const [tags, setTags] = useState<Tag[]>([])
-  const { uploadToS3 } = useS3Upload()
-  const [preview, setPreview] = useState('')
-  const [btnState, setBtnState] = useState<'loading' | 'error' | 'success' | null>(null)
+export default function Upload({ orgId }: { orgId: string }) {
+  const { data: session } = useSession();
+  const [allTags, setAllTags] = useState<TagWithImages[]>([]);
+  const [files, setFiles] = useState<File[]>([]);
+  const [tags, setTags] = useState<Tag[]>([]);
+  const { uploadToS3 } = useS3Upload();
+  const [preview, setPreview] = useState("");
+  const [btnState, setBtnState] = useState<
+    "loading" | "error" | "success" | null
+  >(null);
 
-  const titleRef = useRef<HTMLInputElement>(null)
-  const locationRef = useRef<HTMLInputElement>(null)
-  const dateRef = useRef<HTMLInputElement>(null)
-  const descRef = useRef<HTMLTextAreaElement>(null)
-  const altRef = useRef<HTMLInputElement>(null)
+  const titleRef = useRef<HTMLInputElement>(null);
+  const locationRef = useRef<HTMLInputElement>(null);
+  const dateRef = useRef<HTMLInputElement>(null);
+  const descRef = useRef<HTMLTextAreaElement>(null);
+  const altRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    fetch('/api/tag')
+    fetch("/api/tag")
       .then((res) => res.json())
-      .then((data) => setAllTags(data))
-  }, [tags])
+      .then((data) => setAllTags(data));
+  }, [tags]);
 
   const clearForm = function () {
-    setPreview('')
-    setFiles([])
-    setTags([])
-    titleRef.current ? (titleRef.current.value = '') : null
-    locationRef.current ? (locationRef.current.value = '') : null
-    dateRef.current ? (dateRef.current.value = '') : null
-    descRef.current ? (descRef.current.value = '') : null
-    altRef.current ? (altRef.current.value = '') : null
-  }
+    setPreview("");
+    setFiles([]);
+    setTags([]);
+    titleRef.current ? (titleRef.current.value = "") : null;
+    locationRef.current ? (locationRef.current.value = "") : null;
+    dateRef.current ? (dateRef.current.value = "") : null;
+    descRef.current ? (descRef.current.value = "") : null;
+    altRef.current ? (altRef.current.value = "") : null;
+  };
 
-  const handleSubmit = async function (event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    setBtnState('loading')
+  const handleSubmit = async function (
+    event: React.FormEvent<HTMLFormElement>
+  ) {
+    event.preventDefault();
+    setBtnState("loading");
     try {
-      const { url } = await uploadToS3(files[0])
-      const res = await fetch('/api/image', {
-        method: 'POST',
+      const { url } = await uploadToS3(files[0]);
+      const res = await fetch("/api/image", {
+        method: "POST",
         body: JSON.stringify({
-          user: session && session.user.id,
+          orgId: orgId,
           title: titleRef.current?.value,
           location: locationRef.current?.value,
           date: dateRef.current?.value,
@@ -72,67 +79,99 @@ export default function Upload() {
           tagIds: tags.map((tag) => tag.id),
         }),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-      })
-      const status = res.status
-      const data = await res.json()
+      });
+      const status = res.status;
+      const data = await res.json();
       if (status === 200) {
-        setBtnState('success')
-        clearForm()
+        setBtnState("success");
+        clearForm();
       }
-      console.log(data)
+      console.log(data);
     } catch (error) {
-      setBtnState('error')
-      console.log(error)
+      setBtnState("error");
+      console.log(error);
     }
-  }
+  };
 
   return (
     <div className={s.upload}>
       <form>
         <h2>Upload Image</h2>
-        <label htmlFor='title'>Image title</label>
-        <input ref={titleRef} type='text' name='title' id='title' />
+        <label htmlFor="title">Image title</label>
+        <input ref={titleRef} type="text" name="title" id="title" />
         <div className={s.inputGroup}>
           <div className={s.location}>
-            <label htmlFor='location'>Location</label>
-            <input ref={locationRef} type='text' name='location' id='location' />
+            <label htmlFor="location">Location</label>
+            <input
+              ref={locationRef}
+              type="text"
+              name="location"
+              id="location"
+            />
           </div>
           <div className={s.date}>
-            <label htmlFor='date'>Date</label>
-            <input ref={dateRef} type='date' name='date' id='date' />
+            <label htmlFor="date">Date</label>
+            <input ref={dateRef} type="date" name="date" id="date" />
           </div>
         </div>
-        <label htmlFor='description'>Description</label>
-        <textarea ref={descRef} name='description' id='description' />
-        <label htmlFor='alt'>Image alt text</label>
-        <input ref={altRef} type='text' name='alt' id='alt' />
+        <label htmlFor="description">Description</label>
+        <textarea ref={descRef} name="description" id="description" />
+        <label htmlFor="alt">Image alt text</label>
+        <input ref={altRef} type="text" name="alt" id="alt" />
         <Tags tags={tags} setTags={setTags} allTags={allTags} />
-        <Dropzone setFiles={setFiles} setPreview={setPreview} preview={preview} />
-        <p>{btnState === 'error' ? 'File was not uploaded. Please try again' : btnState === 'success' ? 'File uploaded successfully!' : null}</p>
+        <Dropzone
+          setFiles={setFiles}
+          setPreview={setPreview}
+          preview={preview}
+        />
+        <p>
+          {btnState === "error"
+            ? "File was not uploaded. Please try again"
+            : btnState === "success"
+            ? "File uploaded successfully!"
+            : null}
+        </p>
         <div className={s.btnGroup}>
-          <Btn onClick={() => setPreview('')}>Cancel</Btn>
+          <Btn onClick={() => clearForm()}>Cancel</Btn>
           <BoldBtn onClick={handleSubmit}>
-            {btnState === 'loading' ? <Loader size='16' /> : null}
-            {btnState === 'error' ? 'Upload' : null}
-            {btnState === 'success' ? 'Success!' : null}
-            {btnState === null ? 'Upload' : null}
+            {btnState === "loading" ? <Loader size="16" /> : null}
+            {btnState === "error" ? "Upload" : null}
+            {btnState === "success" ? "Success!" : null}
+            {btnState === null ? "Upload" : null}
           </BoldBtn>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-// export async function getServerSideProps({ req, res }: { req: NextApiRequest; res: NextApiResponse }) {
-//   res.setHeader('Cache-Control', 'no-cache')
-//   const tags = await prisma.tag.findMany()
-//   // console.log(tags)
+export async function getServerSideProps(context: {
+  req: NextApiRequest;
+  res: NextApiResponse;
+  authOptions: NextAuthOptions;
+}) {
+  const session: Session | null = await getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
 
-//   return {
-//     props: {
-//       allTags: tags,
-//     },
-//   }
-// }
+  if (!session) return { props: {} };
+
+  const org = await prisma.organization.findFirst({
+    where: {
+      user: {
+        id: session.user.id,
+      },
+    },
+  });
+  const orgId = org?.id;
+
+  return {
+    props: {
+      orgId,
+    },
+  };
+}

3d887d703e03afa927f68366d45b696ee8ccfa1d
dejmedus <email96123@gmail.com>
2023-05-26 11:57:32 -0600

    wrap update in try catch

--- pages/api/account/onboarding.ts
+++ pages/api/account/onboarding.ts
@@ -11,31 +11,37 @@ export default async function handle(
   const session = await getServerSession(req, res, authOptions);
 
   if (session) {
-    // update user role
-    if (isOrg) {
-      const updateUser = await prisma.user.update({
-        where: {
-          id: session.user?.id,
-        },
-        data: {
-          role: "ORG",
-          organization: {
-            create: {
-              name: orgName,
-              description: orgDesc,
+    try {
+      if (isOrg) {
+        // update user role and add org data
+        const updateUser = await prisma.user.update({
+          where: {
+            id: session.user?.id,
+          },
+          data: {
+            role: "ORG",
+            organization: {
+              create: {
+                name: orgName,
+                description: orgDesc,
+              },
             },
           },
-        },
-      });
-    } else {
-      const updateUser = await prisma.user.update({
-        where: {
-          id: session.user?.id,
-        },
-        data: {
-          role: "USER",
-        },
-      });
+        });
+      } else {
+        // update user role
+        const updateUser = await prisma.user.update({
+          where: {
+            id: session.user?.id,
+          },
+          data: {
+            role: "USER",
+          },
+        });
+      }
+      res.status(200).send({ message: "Successfully created an account" });
+    } catch (error) {
+      res.status(405).send(error);
     }
   } else {
     res.status(401).send({ message: "Unauthorized" });

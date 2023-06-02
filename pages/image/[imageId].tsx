233978a5c163e3b1e2d5e50ba6d41c80736704ad
dejmedus <email96123@gmail.com>
2023-06-02 12:57:53 -0600

    intial download function

--- pages/image/[imageId].tsx
+++ pages/image/[imageId].tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import prisma from "@/lib/prisma";
 import { BoldBtn } from "@/components/Buttons/Buttons";
@@ -17,18 +18,56 @@ interface IImage {
 }
 
 function SelectedImage({ image }: { image: IImage | null }) {
+  const [isDownloading, setIsDownloading] = useState(false);
   if (!image || image.url == null) return <h3>No image found</h3>;
 
   return (
     <div>
-      <Image src={image.url} alt={image.alt} width={400} height={400} />
+      <Image
+        src={image.url}
+        alt={image.alt}
+        width={400}
+        height={400}
+        priority={true}
+      />
       <h4>{image.title}</h4>
-      <BoldBtn onClick={() => console.log("download image")}>Download</BoldBtn>
+      <BoldBtn
+        onClick={() => download(image.url, image.title, setIsDownloading)}
+        disabled={isDownloading}
+      >
+        Download
+      </BoldBtn>
     </div>
   );
 }
 
 export default SelectedImage;
+
+async function download(
+  imageUrl: string,
+  imageName: string,
+  setIsDownloading: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  setIsDownloading(true);
+
+  try {
+    const image = await fetch(imageUrl);
+    const imageBlob = await image.blob();
+    console.log(imageBlob);
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(imageBlob);
+    link.download = `${imageName}.png`;
+
+    link.setAttribute("aria-hidden", "true");
+    link.style.display = "none";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } catch (error) {
+    console.error("Error downloading image", error);
+  }
+}
 
 export async function getServerSideProps(context: {
   query: { imageId: string };

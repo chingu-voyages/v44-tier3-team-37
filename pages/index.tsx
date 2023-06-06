83cf5b60a0f36c4a2f4c9905b278cb36be014169
James Lee <jamesleex86@gmail.com>
2023-06-06 10:36:21 -0700

    Added edit icon for org images

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,6 +93,7 @@ const Home: React.FC<ImagesProps> = ({ allImages, organizationImages, userImages
             {organizationImages?.map(image => (
               <div className={s.imageContainer} key={image.id}>
                 <img src={image.url} className={s.image} alt={image.alt} />
+                <svg xmlns="http://www.w3.org/2000/svg" className={s.favoriteIcon} width="23" height="23" viewBox="0 0 24 24" fill="none" stroke="#6eadf4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="16 3 21 8 8 21 3 21 3 16 16 3"></polygon></svg>
               </div>
             ))}
           </div>
@@ -104,7 +105,7 @@ const Home: React.FC<ImagesProps> = ({ allImages, organizationImages, userImages
                 <img src={image.url} className={s.image} alt={image.alt} />
                 {favorited(image.id)
                   ?
-                  <svg xmlns="http://www.w3.org/2000/svg" onClick={() => updateUserCollection(image.id)} className={s.favoriteIcon} width="23" height="23" viewBox="0 0 24 24" fill="#5b7aa4" stroke="#5b7aa4" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" onClick={() => updateUserCollection(image.id)} className={s.favoriteIcon} width="23" height="23" viewBox="0 0 24 24" fill="#6eadf4" stroke="#6eadf4" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                   :
                   <svg xmlns="http://www.w3.org/2000/svg" onClick={() => updateUserCollection(image.id)} className={s.favoriteIcon} width="23" height="23" viewBox="0 0 24 24" fill="none" stroke="#6eadf4" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                 }
